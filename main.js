50c635cfcd8e29ca2aec1ba6924c122af53b00b8
Thisma10 <math.cutullic@gmail.com>
2021-08-06 11:05:13 +0200

    Changement sur le bandeau defilant

---
+++ main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+    // Votre code ici avec les appels à la fonction $()
+    
+    // Load this when the DOM is ready
+$(function(){
+  // You used .myCarousel here. 
+  // That's the class selector not the id selector,
+  // which is #myCarousel
+  $('#carouselExampleInterval').carousel();
+});
+ });
+
+
+
