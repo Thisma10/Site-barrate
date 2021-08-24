ad4449eb5f3387c3056ff7a8560959263d3905e3
Thisma10 <math.cutullic@gmail.com>
2021-08-24 09:41:52 +0200

    prettier

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     // Votre code ici avec les appels à la fonction $()
-    
+
     // Load this when the DOM is ready
-$(function(){
-  // You used .myCarousel here. 
-  // That's the class selector not the id selector,
-  // which is #myCarousel
-  $('#carouselExampleInterval').carousel();
+    $(function () {
+        // You used .myCarousel here.
+        // That's the class selector not the id selector,
+        // which is #myCarousel
+        $("#carouselExampleInterval").carousel();
+    });
 });
- });
 
-
-
-
- $(function(){
-  $(".myPopover").popover();
+$(function () {
+    $(".myPopover").popover();
 });
 
 
-
-    
